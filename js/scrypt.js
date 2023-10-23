23455479144f2a043ce29f48f03ae685324706bd
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-10-23 20:26:31 +0300

    999

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,32 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function loadImagesFromFolder(folderPath) {
-        fetch(folderPath)
-            .then(response => response.text())
+    function loadImagesFromServer() {
+        fetch('/photos')
+            .then(response => response.json())
             .then(data => {
-                const files = data.split("\n").filter(Boolean).map(fileName => `${folderPath}/${fileName}`);
-                if (files.length > 0) {
-                    files.forEach(function(imageSrc) {
+                if (data.length > 0) {
+                    data.forEach(function(imageSrc) {
                         const column = document.querySelector(`.column:nth-child(${Math.floor(Math.random() * 3) + 1})`);
                         column.appendChild(createImage(imageSrc));
                     });
                 } else {
                     const message = document.createElement("p");
-                    message.textContent = "В папці немає фотографій.";
+                    message.textContent = "На сервері немає фотографій.";
                     document.body.appendChild(message);
                 }
             })
             .catch(error => console.error('Error:', error));
     }
 
-    function createImage(src) {
-        const img = document.createElement("img");
-        img.src = src;
-        img.alt = "Фото";
-        img.style.maxHeight = "300px";
-        img.style.marginBottom = "10px";
-        return img;
-    }
+    // Решта коду залишається незмінним
 
-    const gallery = document.querySelector(".gallery");
-    loadImagesFromFolder("img"); // Шлях до вашої папки з фотографіями
+    loadImagesFromServer(); // Замість loadImagesFromFolder("img")
 });
