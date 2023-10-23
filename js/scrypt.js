2c171e8231e7c4bea37a2a4be4e7f9bce65c0ffa
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-10-23 19:16:29 +0300

    Update scrypt.js

--- js/scrypt.js
+++ js/scrypt.js
@@ -11,6 +11,35 @@ for (const photoContainer of photoContainers) {
 
   // Розраховуємо ширину кожного фото
   const photoWidth = photoContainer.offsetWidth / photoHeights.length;
+// Отримуємо всі div-контейнери з фото
+const photoContainers = document.querySelectorAll(".col-lg-6");
+
+// Обробляємо кожен div-контейнер з фото
+for (const photoContainer of photoContainers) {
+  // Отримуємо висоту першого фото в колонці
+  const firstPhotoHeight = getImageHeight(photoContainer.querySelector("img"));
+
+  // Створюємо масив з висотами всіх зображень в колонці
+  const photoHeights = [];
+  for (const img of photoContainer.querySelectorAll("img")) {
+    photoHeights.push(getImageHeight(img));
+  }
+
+  // Розраховуємо ширину кожного фото
+  const photoWidth = photoContainer.offsetWidth / photoHeights.length;
+
+  // Розміщуємо кожне фото в колонці на однаковій відстані від верхнього краю контейнера
+  for (let i = 0; i < photoHeights.length; i++) {
+    const photo = photoContainer.querySelector("img:nth-child(" + (i + 1) + ")");
+    photo.style.height = `${firstPhotoHeight - photoHeights[i]}px`;
+    photo.style.left = `${photoWidth * i}px`;
+  }
+}
+
+// Отримуємо висоту зображення
+function getImageHeight(img) {
+  return img.getBoundingClientRect().height;
+}
 
   // Розміщуємо кожне фото в колонці на лінії з першим фото
   photoHeights.forEach((photoHeight, index) => {
