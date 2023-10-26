a342ef2b355b889f784b7d634c0bf533f18fb225
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-10-26 21:50:22 +0300

    1

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,22 +1,13 @@
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+const fs = require('fs');
 
-public class Main {
-    public static void main(String[] args) {
-        File folder = new File("./imgs"); // Шлях до директорії з файлами
+const folderPath = './imgs'; // Шлях до директорії з файлами
 
-        File[] files = folder.listFiles(); // Отримуємо список файлів
-        List<String> jpgs = new ArrayList<>(); // Створюємо список для файлів з розширенням .jpg
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error('Помилка при читанні директорії:', err);
+    return;
+  }
 
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile() && file.getName().endsWith(".jpg")) {
-                    jpgs.add(file.getName());
-                }
-            }
-        }
-
-        System.out.println(jpgs.size());
-    }
-}
+  const jpgs = files.filter(file => file.endsWith('.jpg'));
+  console.log(jpgs.length);
+});
